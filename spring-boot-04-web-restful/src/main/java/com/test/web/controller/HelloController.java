7d5b25fc7fb5e39696acbb38d587fc8f2757f7ca
曾雯倩 <wenqian.zeng@hand-china.com>
2019-08-26 15:44:56 +0800

    [ADD]yml,cinfig

--- spring-boot-04-web-restful/src/main/java/com/test/web/controller/HelloController.java
+++ spring-boot-04-web-restful/src/main/java/com/test/web/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.test.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,12 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author 24360 2019/08/19 16:40
  */
-@RestController
+@Controller
 public class HelloController {
 
+    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "hello world ! " ;
     }
 
+    @RequestMapping("/success")
+    public String success(){
+        return "getSuccess" ;
+    }
 }
